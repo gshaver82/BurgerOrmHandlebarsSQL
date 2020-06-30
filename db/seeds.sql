822b7bbbd26447dbce617f391890d411ea43874d
gshaver <geneshaver@gmail.com>
2020-06-30 00:52:24 -0500

    Starter code based on cats example

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burger_db;
+
+
+INSERT INTO burgers (burger_name) VALUES ('beef');
+INSERT INTO burgers (burger_name) VALUES ('chicken');
+INSERT INTO burgers (burger_name) VALUES ('CoronaVirus');
+INSERT INTO burgers (burger_name) VALUES ('Sloppy jo');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('booltest', FALSE);
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,64 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".change-sleep").on("click", function(event) {
+    var id = $(this).data("id");
+    //this is true if not eaten
+    var newSleep = $(this).data("newsleep");
+
+    console.log(newSleep);
+
+    var newSleepState = {
+      sleepy: "FALSE"
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newSleepState
+    }).then(
+      function() {
+        console.log("changed sleep to", newSleep);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+
+    var newBurger = {
+      burger_name: $("#ca").val().trim(),
+      devoured: 0,
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
