9e3488d3ae149075456d23f84c27b13fc32e9010
gshaver <geneshaver@gmail.com>
2020-06-30 22:37:14 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@
 var mysql = require("mysql");
 
 //heroku connection
-var connection = mysql.createConnection({
-  host: process.env.HOST,
-  port: 3306,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE
-});
+// var connection = mysql.createConnection({
+//   host: process.env.HOST,
+//   port: 3306,
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   database: process.env.DATABASE
+// });
 
 // var connection = mysql.createConnection({
 //   host: "localhost",
@@ -18,8 +18,21 @@ var connection = mysql.createConnection({
 //   database: "burger_db"
 // });
 
+
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1Redsql",
+    database: "burger_db"
+  });
+}
+
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
